c7f10a06a9400050b685001edbf8025a33882c45
Wataru Kurashima <what.r.j150@gmail.com>
2022-03-19 07:05:59 +0000

    feat(telemetry): separate telemetry.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::fs::Metadata;
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+pub mod telemetry;
 
 pub trait Log: Sync + Send {
     fn enabled(&self, metadata: &Metadata) -> bool;
