c60a2dc3d43e99041b8302c10723215f5d349417
Wataru Kurashima <what.r.j150@gmail.com>
2022-03-19 04:52:39 +0000

    feat(logger): add logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 //! src/lib.rs
+
+use std::fs::Metadata;
+
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+
+pub trait Log: Sync + Send {
+    fn enabled(&self, metadata: &Metadata) -> bool;
+
+    // fn log(&self, record: &Record);
+
+    fn flush(&self);
+}
