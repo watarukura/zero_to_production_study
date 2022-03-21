9a7b15305f98bd5eedc88eaa6ba0c125ca94568a
Wataru Kurashima <what.r.j150@gmail.com>
2022-03-21 05:34:52 +0000

    feat(configuration): separate configurations by environments

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,7 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -41,8 +41,59 @@ impl DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
+    let configuration_directory = base_path.join("configuration");
 
-    settings.merge(config::File::with_name("configuration"))?;
+    // Read the "default" configuration file
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    // Layer on the environment-specific value
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
 
     settings.try_into()
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+pub enum Environment {
+    Local,
+    Production,
+    CI,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+            Environment::CI => "ci",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            "ci" => Ok(Self::CI),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production` or `ci`.",
+                other
+            )),
+        }
+    }
 }
